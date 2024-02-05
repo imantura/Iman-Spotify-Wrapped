d3271ff5598fc297344b27ef6e19a6dc6d3bf1aa
imantura <143624557+imantura@users.noreply.github.com>
2024-02-05 01:11:33 -0500

    Add files via upload
    All files

---
+++ src/artists.jsx
@@ -0,0 +1,63 @@
+/* 
+This file contains the artist data. DO NOT CHANGE
+*/
+const artists = [
+    {
+        
+        name: 'Welcome!',
+        minutes: null,
+        songs: ['Do you want to see some artists and songs, I enjoyed in 2023','Please hit the white heart on your left to go to the previous artist', 'Please hit the white heart on your right to go to the next artist'],
+        image_src: 'images/welcome.jpg'
+    },
+    {
+        
+        name: 'ABBA',
+        minutes: 1860,
+        songs: ['SOS', 'Hasta Manana','Angeleyes','The Day Before You Came','Knowing me, Knowing You'],
+        image_src: 'images/ABBA.jpg'
+    },
+    {
+        name: 'Bob Marley',
+        minutes: 3600,
+        songs: ['No Woman No Cry', 'Misty Morning','Jamming', 'Could You Be Loved','Three Little Birds' ],
+        image_src: 'images/Marley.jpg'
+    },
+    {
+        name: 'TWICE',
+        minutes: 525600,
+        songs: ['Fancy', 'Feel Special', "Fake & True", 'Trick It', 'Kura Kura'],
+        image_src: 'images/sana.jpg'
+    },
+    {
+        name: 'Omah Lay',
+        minutes: 3000,
+        songs: ['attention', 'soso', 'Soweto', 'People', 'understand'],
+        image_src: 'images/OmahLay.jpg'
+    },
+    {
+        name: 'Sade',
+        minutes: 7555,
+        songs: ['No Ordinary Love', 'Smooth Opperator', 'Kiss of Life', 'The Moon and the Sky', 'Like a Tattoo'],
+        image_src: 'images/Sade.jpg'
+    }, 
+    {
+        name: 'Tupac',
+        minutes: 700,
+        songs: ['Changes', 'Do For Love', 'Pain', 'Hit Em Up', 'All Eyez On Me'],
+        image_src: 'images/Tupac.jpg'
+    }, 
+    {
+        name: 'Diana Ross',
+        minutes: 2020,
+        songs: ['I\'m Coming Out', 'Upside Down', 'Endless Love', 'It\'s My House', 'Ain\'t No Mountain'],
+        image_src: 'images/DiannaRoss.jpg'
+    }, 
+    {
+        name: 'Whitney Huston',
+        minutes: 3100,
+        songs: ['I Have Nothing', 'How Will I Know', 'My Name Is Not Susan', 'Higher Love', 'My Love Is Your Love'],
+        image_src: 'images/Whitney.jpg'
+    } 
+]
+
+export default artists
